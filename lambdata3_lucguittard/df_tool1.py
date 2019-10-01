e3807397d213af94e4724faa392aa32c352a8496
lucguittard <lucguittard@users.noreply.github.com>
2019-10-01 16:07:55 -0600

    new lambdata with mod 2 class commit

---
+++ lambdata3_lucguittard/df_tool1.py
@@ -0,0 +1,44 @@
+"""
+pack2 - a limited set of df engineering tools
+"""
+
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+
+
+# code to train/test/val split a df
+
+
+
+def splitter(df):
+    train, test = train_test_split(df, train_size=.8)
+    train, val = train_test_split(train, train_size=.8)
+    print(train, test, val)
+
+
+def detail_na(df):
+    nas = df.isna().sum()
+    print(nas)
+
+
+# code to make a class
+
+
+class Complex:
+    """ 
+    Class for complex numbers 
+    """
+    def __init__(self, realpart, imagpart):        
+        self.r = realpart
+        self.i = imagpart
+
+    """ 
+    Method to add together two complex numbers 
+    """
+    def add(self):
+        self.added = (self.r+self.i)
+
+x = Complex(3.0, -4.5)
+x.r, x.i
+
